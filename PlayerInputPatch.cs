3850fd55795508dafa9ae4622b6f6bc1cad41dd9
wenright <wenright0@gmail.com>
2025-07-01 22:49:31 -0500

    initial commit

---
+++ PlayerInputPatch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace StickAngleResetter;
+
+[HarmonyPatch(typeof(PlayerInput))]
+public class PlayerInputPatch : IPuckMod
+{
+  static readonly Harmony harmony = new Harmony("wenright.StickAngleResetter");
+
+  private static Mouse mouse;
+
+  [HarmonyPostfix]
+  [HarmonyPatch("Start")]
+  public static void PostfixStart(PlayerInput __instance, ref float ___bladeAngleBuffer)
+  {
+    mouse = Mouse.current;
+  }
+
+  [HarmonyPostfix]
+  [HarmonyPatch("Update")]
+  public static void PostfixUpdate(PlayerInput __instance, ref float ___bladeAngleBuffer)
+  {
+    if (mouse != null && mouse.middleButton.wasReleasedThisFrame)
+    {
+      __instance.BladeAngleInput.ClientValue = 0;
+      ___bladeAngleBuffer = 0;
+    }
+  }
+
+  public bool OnEnable()
+  {
+    try
+    {
+      harmony.PatchAll();
+    }
+    catch (Exception e)
+    {
+      Debug.LogError($"Harmony patch failed: {e.Message}");
+
+      return false;
+    }
+
+    return true;
+  }
+
+  public bool OnDisable()
+  {
+    try
+    {
+      harmony.UnpatchSelf();
+    }
+    catch (Exception e)
+    {
+      Debug.LogError($"Harmony unpatch failed: {e.Message}");
+
+      return false;
+    }
+
+    return true;
+  }
+}
